9b8394eb7a6f53beae2072309756c091c69d7bc2
Jan Kubicek <143211313+Jan-Kubicek@users.noreply.github.com>
2023-09-27 14:15:06 +0200

    Add files via upload

--- haskell-1.hs
+++ haskell-1.hs
@@ -1,3 +1,4 @@
+-- Škola Komentář
 nasobeni a b = a * b
 cel x = x/(5/9) + 32  -- z F => C
 far x = (5/9)*(x-32)  -- z C => F 
@@ -5,3 +6,13 @@ absolute a = if(a < 0) then a * (-1) else a
 negation a = if(a) then False else True
 myAnd a b = if( ( a  == True )  &&  ( b == True ) ) then True else False  
 myOr a b = if(( a == True ) || ( b == True)) then True else False
+vetsi2 a b = if(a > b) then a else b
+mensi2 a b = if(a<b) then a else b
+mocnina a b = abs(a)^abs(b)
+dvojnasobek a = 2 * a
+soucetCtvercu a b = (a ^ 2) + (b ^ 2)
+soucetCtvercU a b = (mocnina a 2) + (mocnina b 2)
+jemensi a b = a < b
+mensi a b = if(a < b) then a else b
+vetsi a b = if(a >b) then a else b
+logickySoucet a b = if a then True else if b then True else False
